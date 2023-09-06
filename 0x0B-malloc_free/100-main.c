420a1c6d2826ce363231bbbccac8b98f65b94bae
stanleydeveloper <stanleylove769@gmail.com>
2023-09-05 17:23:31 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -1,41 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * argstostr -  The main main entry
- * @ac: int input
- * @av: Double pointer array
- * Return: 0
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
  */
-char *argstostr(int ac, char **av)
+int main(int ac, char *av[])
 {
-	int i, n, r = 0, l = 0;
-	char *str;
+    char *s;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (n = 0; av[i][n]; n++)
-			l++;
-	}
-	l += ac;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-	for (n = 0; av[i][n]; n++)
-	{
-		str[r] = av[i][n];
-		r++;
-	}
-	if (str[r] == '\0')
-	{
-		str[r++] = '\n';
-	}
-	}
-	return (str);
+    s = argstostr(ac, av);
+    if (s == NULL)
+    {
+        return (1);
+    }
+    printf("%s", s);
+    free(s);
+    return (0);
 }
